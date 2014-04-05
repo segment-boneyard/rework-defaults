8a532b13d7127d45b85e8664128dedc61776b97c
Amir Abu Shareb <yields@icloud.com>
2014-04-05 08:29:26 +0300

    dedupe

--- test/test.js
+++ test/test.js
@@ -8,12 +8,19 @@ var assert = require('assert');
 describe('rework-defaults', function(){
   it('should work', function(){
     var out = rework(input).use(defaults({ width: '100px' }));
-    assert(expected.trim() == out.toString().trim());
+    var actual = out.toString().trim();
+    if (actual == expected) return;
+    var err = new Error('Error');
+    err.actual = actual;
+    err.expected = expected;
+    err.showDiff = true;
+    throw err;
   });
 });
 
 function read(name){
   return require('fs')
     .readFileSync('test/fixtures/' + name)
-    .toString();
+    .toString()
+    .trim();
 }
